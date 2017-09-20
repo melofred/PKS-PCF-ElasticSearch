98fa83b45726b30c01fed3c212f957e53793dacc
Fred Melo <fmelo@Freds-MacBook-Pro.local>
2017-09-19 22:32:32 -0700

    fixes

--- Elasticsearch-sink/src/main/java/io/pivotal/example/stream/elasticsearch/sink/ElasticsearchSInkConfiguration.java
+++ Elasticsearch-sink/src/main/java/io/pivotal/example/stream/elasticsearch/sink/ElasticsearchSInkConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
 
 import io.pivotal.example.stream.elasticsearch.sink.service.Earthquake;
 import io.pivotal.example.stream.elasticsearch.sink.service.ElasticsearchPersistenceService;
@@ -21,18 +22,23 @@ public class ElasticsearchSInkConfiguration {
 	private ElasticsearchPersistenceService svc;
 
 	@StreamListener(Sink.INPUT)
+	/*
 	public void handle(Earthquake quake) {
 		svc.insert(quake);
 	}
-	
-/*	
+	*/
+
 	public void onMessage(Message<?> message) {
 		
 		String payload = message.getPayload().toString();
+		svc.insert(svc.parse(payload));
+		 
 		
 		
 	}
-	*/
+	
+	
+	
 	
 	
 }
--- Elasticsearch-sink/src/main/java/io/pivotal/example/stream/elasticsearch/sink/service/ElasticsearchPersistenceService.java
+++ Elasticsearch-sink/src/main/java/io/pivotal/example/stream/elasticsearch/sink/service/ElasticsearchPersistenceService.java
@@ -1,5 +1,11 @@
 package io.pivotal.example.stream.elasticsearch.sink.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.StringTokenizer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +15,27 @@ public class ElasticsearchPersistenceService {
 	@Autowired
 	private EarthquakeRepository repository;	
 	
+	private DateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SS");
+	
 	public void insert(Earthquake quake) {
 		repository.save(quake);
+	}
+
+	/*
+	 *       DateTime,Latitude,Longitude,Depth,Magnitude,MagType,NbStations,Gap,Distance,RMS,Source,EventID
+	 */
+	public Earthquake parse(String csv) throws ParseException{
+		Earthquake quake = new Earthquake();
+		
+		StringTokenizer tokenizer = new StringTokenizer(csv, ",");
+		
+		quake.setTimestamp(formatter.parse(tokenizer.nextToken()));
+		quake.setLatitude(Float.valueOf(tokenizer.nextToken()));
+		quake.setLongitude(Float.valueOf(tokenizer.nextToken()));
+		quake.setDepth(depth);
+		quake.setMagnitude(magnitude);
+		quake.set
+		return quake;
 	}
 	
 	
